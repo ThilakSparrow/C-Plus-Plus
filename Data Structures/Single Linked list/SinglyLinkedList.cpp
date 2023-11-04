ba86a23a52fa8ffb0960a36c2ef8de475f758247
Thilak Sparrow <93604124+ThilakSparrow@users.noreply.github.com>
2023-11-04 15:32:42 +0530

    Create SinglyLinkedList.cpp

---
+++ Data Structures/Single Linked list/SinglyLinkedList.cpp
@@ -0,0 +1,307 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+
+{
+
+    int data;
+
+    struct node* next;
+
+};
+
+int n, cnt=0;
+
+struct node *first, *temp, *p;
+
+void create()
+
+{
+
+    first=new node;
+
+    cout<<"\n\t ENTER DATA : ";
+
+    cin>>n;
+
+    first->data=n;
+
+    first->next=NULL;
+
+    cnt=1;
+
+}
+
+void insertfirst()
+
+{
+
+    p=new node;
+
+    cout<<"\n\t ENTER DATA FOR 1ST NODE";
+
+    cin>>n;
+
+    p->data=n;
+
+    p->next=first;
+
+    first=p;
+
+    cnt++;
+
+    cout<<"\tELEMENT SUCCESSFULLY ADDED";
+
+}
+
+void insertlast()
+
+{
+
+    p=new node;
+
+    cout<<"\n\t ENTER DATA FOR LAST NODE";
+
+    cin>>n;
+
+    p->data=n;
+
+    temp=first;
+
+    while(temp->next!=0)
+
+    {
+
+        temp=temp->next;
+
+    }
+
+    temp->next=p;
+
+    p->next=0;
+
+    cnt++;
+
+    cout<<"\tELEMENT SUCCESSFULLY ADDED";
+
+}
+
+void insertafter()
+
+{
+
+    int add,t;
+
+    cout<<"\n\t ENTER ANY ADDRESS BETWEEN 1 and "<<cnt;
+
+    cin>>add;
+
+    t=1;
+
+    p=first;
+
+    while(t!=add) //while(t!=add-1) Before
+
+    {
+
+        p=p->next;
+
+        t++;
+
+    }
+
+    temp=new node;
+
+    cout<<"\n\t ENTER DATA FOR NODE";
+
+    cin>>n;
+
+    temp->data=n;
+
+    temp->next=p->next;
+
+    p->next=temp;
+
+    cnt++;
+
+    cout<<"\tELEMENT SUCCESSFULLY ADDED";
+
+}
+
+void deletefirst()
+
+{
+
+    p=first;
+
+    first=p->next;
+
+    free(p);
+
+    cnt--;
+
+    cout<<"\t ELEMENT SUCCESSFULLY DELETED";
+
+}
+
+void deletelast()
+
+{
+
+    p=first;
+
+    while(p->next->next!=0)
+
+    {
+
+        p=p->next;
+
+    }
+
+    p->next=0;
+
+    free(p->next);
+
+    cnt--;
+
+    cout<<"\tELEMENT SUCCESSFULLY DELETED";
+
+}
+
+void deleteafter()
+
+{
+
+    int add,t;
+
+    cout<<"\n\t ENTER ANY ADDRESS BETWEEN 1 and"<<cnt;
+
+    cin>>add;
+
+    t=1;
+
+    p=first;
+
+    while(t<add-1)
+
+    {
+
+        p=p->next;
+
+        t++;
+
+    }
+
+    temp=p->next;
+
+    p->next=temp->next;
+
+    free(temp);
+
+    cnt--;
+
+    cout<<"\tELEMENT SUCCESSFULLY DELETED";
+
+}
+
+void display()
+
+{
+
+    p=first;
+
+    while(p!=0)
+
+    {
+
+        cout<<"\n\t"<<p->data;
+
+         p=p->next;
+
+    }
+
+    cout<<"\nTotal no. of Nodes present in a list:"<<cnt<<"\n";
+
+}
+
+int main()
+
+{
+
+    int i;
+
+    while(1)
+
+    {
+
+        cout<<"1.Create\t 2.Insert first 3. ...9.exit\n";
+
+        cout<<"Enter Your Choice";
+
+        cin>>i;
+
+        switch(i)
+
+        {
+
+            case 1:
+
+                create();
+
+                break;
+
+            case 2:
+
+                insertfirst();
+
+                break;
+
+            case 3:
+
+                insertlast();
+
+                break;
+
+            case 4:
+
+                insertafter();
+
+                break;
+
+            case 5:
+
+                deletefirst();
+
+                break;
+
+            case 6:
+
+                deletelast();
+
+                break;
+
+            case 7:
+
+                deleteafter();
+
+                break;
+
+            case 8:
+
+                display();
+
+                break;
+
+            default:
+
+                exit(0);
+
+        }
+
+    }
+
+return 0;
+
+}
